ca44387e5ad07556f9227617ffcd7b419551c41a
DaKinder <103947080+DaKinder@users.noreply.github.com>
2023-06-09 11:48:14 +0300

    sem_4 added

--- Seminars/sem_4.py
+++ Seminars/sem_4.py
@@ -7,7 +7,22 @@
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 # Для решения данной задачи используйте функцию .split()
 
-a
+import os
+os.system('clear')
+
+text = input('Введите строку символов')
+myList = text.split()
+myDict = {}
+count = str()
+
+for letter in myList:
+    if letter in myDict.keys():
+        myDict[letter] += 1
+        count += f'{letter}_{myDict[letter]} '
+    else:
+        myDict[letter] = 0
+        count += f'{letter} '
+print(count)
 
 # Задача №27. Решение в группах
 # Пользователь вводит текст(строка). Словом считается
@@ -22,35 +37,10 @@ a
 # Output: 13
 
 
-# Задача №29. Решение в группах
-# Ваня и Петя поспорили, кто быстрее решит
-# следующую задачу: “Задана последовательность
-# неотрицательных целых чисел. Требуется определить
-# значение наибольшего элемента
-# последовательности, которая завершается первым
-# встретившимся нулем (число 0 не входит в
-# последовательность)”. Однако 2 друга оказались не
-# такими смышлеными. Никто из ребят не смог до
-# конца сделать это задание. Они решили так: у кого
-# будет меньше ошибок в коде, тот и выиграл спор. За
-# помощью товарищи обратились к Вам, студентам.
-# Примечание: Программные коды на следующих
-# слайдах
+text = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+print(text)
 
-# Ваня:
-# n = int(input())
-# max_number = 1000
-# while n != 0:
-#  n = int(input())
-#  if max_number > n:
-#  max_number = n
+myList2 = text.upper().split()
 
-# print(max_number)
-# Петя:
-# n = int(input())
-# max_number = -1
-# while n < 0:
-#  n = int(input())
-#  if max_number < n:
-#  n = max_number
-# print(n)
+long = len(set(myList2))
+print(long)
